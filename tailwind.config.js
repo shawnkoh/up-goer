d973427a11f1212d775f752b0f5aa391dd845bde
Shawn Koh <shawn@shawnkoh.sg>
2021-11-18 16:27:16 +0800

    Fix purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const colors = require("tailwindcss/colors");
 
 module.exports = {
   mode: "jit",
-  purge: ["./src/**/*.html", "./src/**/*.js"],
+  purge: ["./src/**/*.html", "./src/**/*.js", "./src/*.res"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
