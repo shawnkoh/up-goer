a2d5e2420c98282cc59c7093ffd5bb0cc4c67bc0
Shawn Koh <shawn@shawnkoh.sg>
2021-11-18 15:10:22 +0800

    F

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require("tailwindcss/colors")
 
 module.exports = {
   mode: 'jit',
@@ -11,6 +12,9 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        rose: colors.rose,
       },
     },
   },
